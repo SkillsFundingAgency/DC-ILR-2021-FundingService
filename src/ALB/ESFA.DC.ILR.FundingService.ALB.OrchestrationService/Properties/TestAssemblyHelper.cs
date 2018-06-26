4d013bfa081480ec5bc4962c2918a245740dbbaf
Mike Childs <mike@mjcdev.co.uk>
2018-06-26 14:14:05 +0100

    Deleted Orchestration Service Libraries, to be resurrected when unified

--- src/ALB/ESFA.DC.ILR.FundingService.ALB.OrchestrationService/Properties/TestAssemblyHelper.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-// Setting internals visiible for unit test purposes
-[assembly: InternalsVisibleTo("ESFA.DC.ILR.FundingService.ALB.Service.Tests")]
-[assembly: InternalsVisibleTo("ESFA.DC.ILR.FundingService.ALB.OrchestrationService.Tests")]
-[assembly: InternalsVisibleTo("ESFA.DC.ILR.FundingService.ALB.FundingOutput.Tests")]
