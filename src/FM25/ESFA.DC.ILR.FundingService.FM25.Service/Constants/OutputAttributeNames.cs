3d94c2f8b3971df8987a5b85a219f14bd2dda2e1
Mike Childs <mike@mjcdev.co.uk>
2018-07-27 15:29:51 +0100

    Output Attribute Names Defined

---
+++ src/FM25/ESFA.DC.ILR.FundingService.FM25.Service/Constants/OutputAttributeNames.cs
@@ -0,0 +1,42 @@
+﻿namespace ESFA.DC.ILR.FundingService.FM25.Service.Constants
+{
+    public static class OutputAttributeNames
+    {
+        // Global
+        public const string UKPRN = "UKPRN";
+        public const string LARSVersion = "LARSVersion";
+        public const string OrgVersion = "OrgVersion";
+        public const string PostcodeDisadvantageVersion = "PostcodeDisadvantageVersion";
+        public const string RulebaseVersion = "RulebaseVersion";
+
+        // Learner
+        public const string LearnRefNumber = "LearnRefNumber";
+        public const string AcadMonthPayment = "AcadMonthPayment";
+        public const string AcadProg = "AcadProg";
+        public const string ActualDaysILCurrYear = "ActualDaysILCurrYear";
+        public const string AreaCostFact1618Hist = "AreaCostFact1618Hist";
+        public const string Block1DisadvUpliftNew = "Block1DisadvUpliftNew";
+        public const string Block2DisadvElementsNew = "Block2DisadvElementsNew";
+        public const string ConditionOfFundingEnglish = "ConditionOfFundingEnglish";
+        public const string ConditionOfFundingMaths = "ConditionOfFundingMaths";
+        public const string CoreAimSeqNumber = "CoreAimSeqNumber";
+        public const string FullTimeEquiv = "FullTimeEquiv";
+        public const string FundLine = "FundLine";
+        public const string LearnerActEndDate = "LearnerActEndDate";
+        public const string LearnerPlanEndDate = "LearnerPlanEndDate";
+        public const string LearnerStartDate = "LearnerStartDate";
+        public const string NatRate = "NatRate";
+        public const string OnProgPayment = "OnProgPayment";
+        public const string PlannedDaysILCurrYear = "PlannedDaysILCurrYear";
+        public const string ProgWeightHist = "ProgWeightHist";
+        public const string ProgWeightNew = "ProgWeightNew";
+        public const string PrvDisadvPropnHist = "PrvDisadvPropnHist";
+        public const string PrvHistLrgProgPropn = "PrvHistLrgProgPropn";
+        public const string PrvRetentFactHist = "PrvRetentFactHist";
+        public const string RateBand = "RateBand";
+        public const string RetentNew = "RetentNew";
+        public const string StartFund = "StartFund";
+        public const string ThresholdDays = "ThresholdDays";
+
+    }
+}
