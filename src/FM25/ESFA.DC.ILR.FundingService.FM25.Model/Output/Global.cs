e4adc85e80d4ec17e32f7dd05bee1ee11d54c73e
Mike Childs <mike@mjcdev.co.uk>
2018-07-30 15:13:15 +0100

    FM25 Funding Output Service and Data Entity Attribute Service Implemented, to be refined

--- src/FM25/ESFA.DC.ILR.FundingService.FM25.Model/Output/Global.cs
+++ src/FM25/ESFA.DC.ILR.FundingService.FM25.Model/Output/Global.cs
@@ -4,7 +4,7 @@ namespace ESFA.DC.ILR.FundingService.FM25.Model.Output
 {
     public class Global
     {
-        public int UKPRN { get; set; }
+        public int? UKPRN { get; set; }
         public string LARSVersion { get; set; }
         public string OrgVersion { get; set; }
         public string PostcodeDisadvantageVersion { get; set; }
--- src/FM25/ESFA.DC.ILR.FundingService.FM25.Service/Output/FundingOutputService.cs
+++ src/FM25/ESFA.DC.ILR.FundingService.FM25.Service/Output/FundingOutputService.cs
@@ -1,15 +1,22 @@
-﻿using ESFA.DC.ILR.FundingService.Interfaces;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using ESFA.DC.OPA.Model.Interface;
 using ESFA.DC.ILR.FundingService.FM25.Model.Output;
 using ESFA.DC.ILR.FundingService.FM25.Service.Constants;
-using ESFA.DC.OPA.Service.Abstract;
+using ESFA.DC.ILR.FundingService.Interfaces;
+using ESFA.DC.OPA.Model.Interface;
+using ESFA.DC.OPA.Service.Interface;
 
 namespace ESFA.DC.ILR.FundingService.FM25.Service.Output
 {
-    public class FundingOutputService : AbstractFundingOutputService, IOutputService<IEnumerable<Global>>
+    public class FundingOutputService : IOutputService<IEnumerable<Global>>
     {
+        private readonly IDataEntityAttributeService _dataEntityAttributeService;
+
+        public FundingOutputService(IDataEntityAttributeService dataEntityAttributeService)
+        {
+            _dataEntityAttributeService = dataEntityAttributeService;
+        }
+
         public IEnumerable<Global> ProcessFundingOutputs(IEnumerable<IDataEntity> dataEntities)
         {
             return dataEntities.Select(MapGlobal);
@@ -19,11 +26,11 @@ namespace ESFA.DC.ILR.FundingService.FM25.Service.Output
         {
             return new Global()
             {
-                LARSVersion = GetAttributeValue(dataEntity, OutputAttributeNames.LARSVersion),
-                OrgVersion = GetAttributeValue(dataEntity, OutputAttributeNames.OrgVersion),
-                PostcodeDisadvantageVersion = GetAttributeValue(dataEntity, OutputAttributeNames.PostcodeDisadvantageVersion),
-                RulebaseVersion = GetAttributeValue(dataEntity, OutputAttributeNames.RulebaseVersion),
-                UKPRN = int.Parse(GetAttributeValue(dataEntity, OutputAttributeNames.UKPRN)),
+                LARSVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.LARSVersion),
+                OrgVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.OrgVersion),
+                PostcodeDisadvantageVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.PostcodeDisadvantageVersion),
+                RulebaseVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.RulebaseVersion),
+                UKPRN = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.UKPRN),
                 Learners = dataEntity
                     .Children?
                     .Where(c => c.EntityName == OutputAttributeNames.Learner)
@@ -36,11 +43,32 @@ namespace ESFA.DC.ILR.FundingService.FM25.Service.Output
         {
             return new Learner()
             {
-                AcadMonthPayment = GetIntAttributeValue(dataEntity, OutputAttributeNames.AcadMonthPayment),
-                AcadProg = GetBoolAttributeValue(dataEntity, OutputAttributeNames.AcadProg),
-                ActualDaysILCurrYear = GetIntAttributeValue(dataEntity, OutputAttributeNames.ActualDaysILCurrYear),
-
-                //TODO : Finish
+                AcadMonthPayment = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.AcadMonthPayment),
+                AcadProg = _dataEntityAttributeService.GetBoolAttributeValue(dataEntity, OutputAttributeNames.AcadProg),
+                ActualDaysILCurrYear = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.ActualDaysILCurrYear),
+                AreaCostFact1618Hist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.AreaCostFact1618Hist),
+                Block1DisadvUpliftNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.Block1DisadvUpliftNew),
+                Block2DisadvElementsNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.Block2DisadvElementsNew),
+                ConditionOfFundingEnglish = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.ConditionOfFundingEnglish),
+                ConditionOfFundingMaths = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.ConditionOfFundingMaths),
+                CoreAimSeqNumber = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.CoreAimSeqNumber),
+                FullTimeEquiv = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.FullTimeEquiv),
+                FundLine = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.FundLine),
+                LearnerActEndDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, OutputAttributeNames.LearnerActEndDate),
+                LearnerPlanEndDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, OutputAttributeNames.LearnerPlanEndDate),
+                LearnerStartDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, OutputAttributeNames.LearnerStartDate),
+                NatRate = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.NatRate),
+                OnProgPayment = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.OnProgPayment),
+                PlannedDaysILCurrYear = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.PlannedDaysILCurrYear),
+                ProgWeightHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.ProgWeightHist),
+                ProgWeightNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.ProgWeightNew),
+                PrvDisadvPropnHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.PrvDisadvPropnHist),
+                PrvHistLrgProgPropn = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.PrvHistLrgProgPropn),
+                PrvRetentFactHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.PrvRetentFactHist),
+                RateBand = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.RateBand),
+                RetentNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.RetentNew),
+                StartFund = _dataEntityAttributeService.GetBoolAttributeValue(dataEntity, OutputAttributeNames.StartFund),
+                ThresholdDays = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.ThresholdDays)
             };
         }
     }
