dd921290cea1bed4c942ee0d71e3637f9123f9f9
Mike Childs <mike@mjcdev.co.uk>
2018-08-07 16:04:20 +0100

    ALB Funding Output Condenser Service and Tests

---
+++ src/ALB/ESFA.DC.ILR.FundingService.ALB.Service/FundingOutputCondenserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.FundingService.ALB.FundingOutput.Model;
+using ESFA.DC.ILR.FundingService.Interfaces;
+
+namespace ESFA.DC.ILR.FundingService.ALB.Service
+{
+    public class FundingOutputCondenserService : IFundingOutputCondenserService<FundingOutputs>
+    {
+        public FundingOutputs Condense(IEnumerable<FundingOutputs> fundingOutputs)
+        {
+            return new FundingOutputs()
+            {
+                Global = fundingOutputs.First().Global,
+                Learners = fundingOutputs.Where(o => o.Learners != null).SelectMany(r => r.Learners).ToArray()
+            };
+        }
+    }
+}
