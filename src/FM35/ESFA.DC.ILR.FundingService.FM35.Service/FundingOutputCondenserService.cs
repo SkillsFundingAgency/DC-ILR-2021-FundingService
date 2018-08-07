2380785d1e253654ce07608fb0cc4bf90ae97acf
Mike Childs <mike@mjcdev.co.uk>
2018-08-07 15:56:49 +0100

    FM35 Funding Output Condenser Service

---
+++ src/FM35/ESFA.DC.ILR.FundingService.FM35.Service/FundingOutputCondenserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ESFA.DC.ILR.FundingService.FM35.FundingOutput.Model;
+using ESFA.DC.ILR.FundingService.Interfaces;
+
+namespace ESFA.DC.ILR.FundingService.FM35.Service
+{
+    public class FundingOutputCondenserService : IFundingOutputCondenserService<FM35FundingOutputs>
+    {
+        public FM35FundingOutputs Condense(IEnumerable<FM35FundingOutputs> fundingOutputs)
+        {
+            return new FM35FundingOutputs()
+            {
+                Global = fundingOutputs.First().Global,
+                Learners = fundingOutputs.Where(o => o.Learners != null).SelectMany(r => r.Learners).ToArray()
+            };
+        }
+    }
+}
