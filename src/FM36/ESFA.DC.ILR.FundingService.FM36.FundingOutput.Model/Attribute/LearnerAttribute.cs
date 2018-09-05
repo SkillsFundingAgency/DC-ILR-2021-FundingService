d9cea7487532087dd404def0d82259f01839b365
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-05 15:49:12 +0100

    57821 - FM36 Output model created.

---
+++ src/FM36/ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model/Attribute/LearnerAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Attribute
+{
+    public class LearnerAttribute
+    {
+        public string LearnRefNumber { get; set; }
+
+        public PriceEpisodeAttribute[] ProceEpisodeAttributes { get; set; }
+
+        public LearningDeliveryAttribute[] LearningDeliveryAttributes { get; set; }
+
+        public HistoricEarningOutputAttributeData[] HistoricEarningOutputAttributeDatas { get; set; }
+    }
+}
---
+++ src/FM36/ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model/Attribute/PriceEpisodeAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Attribute
+{
+    public class PriceEpisodeAttribute
+    {
+        public string PriceEpisodeIdentifier { get; set; }
+
+        public PriceEpisodeAttributeData PriceEpisodeAttributeDatas { get; set; }
+
+        public PriceEpisodePeriodisedAttribute[] PriceEpisodePeriodisedAttributes { get; set; }
+    }
+}
