2e61783b6fbfc9d79b2c4cd6f551d5ec0ec8399a
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-26 11:09:48 +0100

    53169 - Refactored FM25 Attribute Constants

---
+++ src/FM25/ESFA.DC.ILR.FundingService.FM25.Service/Constants/Attributes.cs
@@ -0,0 +1,123 @@
+﻿namespace ESFA.DC.ILR.FundingService.FM25.Service.Constants
+{
+    public static class Attributes
+    {
+        // Entities
+        public const string EntityGlobal = "global";
+        public const string EntityLearner = "Learner";
+        public const string EntityLearnerPeriodisedValues = "LearnerPeriodisedValues";
+        public const string EntityLearningDelivery = "LearningDelivery";
+        public const string EntityDPOutcome = "DPOutcome";
+        public const string EntityLearningDeliveryLARSValidity = "LearningDeliveryLARSValidity";
+
+        // Global
+        public const string AreaCostFactor1618 = "AreaCostFactor1618";
+        public const string DisadvantageProportion = "DisadvantageProportion";
+        public const string HistoricLargeProgrammeProportion = "HistoricLargeProgrammeProportion";
+        public const string LARSVersion = "LARSVersion";
+        public const string OrgVersion = "OrgVersion";
+        public const string ProgrammeWeighting = "ProgrammeWeighting";
+        public const string PostcodeDisadvantageVersion = "PostcodeDisadvantageVersion";
+        public const string RetentionFactor = "RetentionFactor";
+        public const string RulebaseVersion = "RulebaseVersion";
+        public const string SpecialistResources = "SpecialistResources";
+        public const string UKPRN = "UKPRN";
+
+        // Learner
+        public const string LearnRefNumber = "LearnRefNumber";
+        public const string AcadMonthPayment = "AcadMonthPayment";
+        public const string AcadProg = "AcadProg";
+        public const string ActualDaysILCurrYear = "ActualDaysILCurrYear";
+        public const string AreaCostFact1618Hist = "AreaCostFact1618Hist";
+        public const string Block1DisadvUpliftNew = "Block1DisadvUpliftNew";
+        public const string Block2DisadvElementsNew = "Block2DisadvElementsNew";
+        public const string ConditionOfFundingEnglish = "ConditionOfFundingEnglish";
+        public const string ConditionOfFundingMaths = "ConditionOfFundingMaths";
+        public const string CoreAimSeqNumber = "CoreAimSeqNumber";
+        public const string DateOfBirth = "DateOfBirth";
+        public const string EngGrade = "EngGrade";
+        public const string FullTimeEquiv = "FullTimeEquiv";
+        public const string FundLine = "FundLine";
+        public const string LearnerActEndDate = "LearnerActEndDate";
+        public const string LearnerPlanEndDate = "LearnerPlanEndDate";
+        public const string LearnerStartDate = "LearnerStartDate";
+        public const string LrnFAM_ECF = "LrnFAM_ECF";
+        public const string LrnFAM_EDF1 = "LrnFAM_EDF1";
+        public const string LrnFAM_EDF2 = "LrnFAM_EDF2";
+        public const string LrnFAM_EHC = "LrnFAM_EHC";
+        public const string LrnFAM_HNS = "LrnFAM_HNS";
+        public const string LrnFAM_MCF = "LrnFAM_MCF";
+        public const string MathGrade = "MathGrade";
+        public const string NatRate = "NatRate";
+        public const string OnProgPayment = "OnProgPayment";
+        public const string PlanEEPHours = "PlanEEPHours";
+        public const string PlanLearnHours = "PlanLearnHours";
+        public const string PlannedDaysILCurrYear = "PlannedDaysILCurrYear";
+        public const string PostcodeDisadvantageUplift = "PostcodeDisadvantageUplift";
+        public const string ProgWeightHist = "ProgWeightHist";
+        public const string ProgWeightNew = "ProgWeightNew";
+        public const string PrvDisadvPropnHist = "PrvDisadvPropnHist";
+        public const string PrvHistLrgProgPropn = "PrvHistLrgProgPropn";
+        public const string PrvRetentFactHist = "PrvRetentFactHist";
+        public const string RateBand = "RateBand";
+        public const string RetentNew = "RetentNew";
+        public const string StartFund = "StartFund";
+        public const string ThresholdDays = "ThresholdDays";
+        public const string ULN = "ULN";
+
+        // Learner Periodised Values
+        public const string LnrOnProgPay = "LnrOnProgPay";
+        public const string Period1 = "LnrPrd1Pay";
+        public const string Period2 = "LnrPrd2Pay";
+        public const string Period3 = "LnrPrd3Pay";
+        public const string Period4 = "LnrPrd4Pay";
+        public const string Period5 = "LnrPrd5Pay";
+        public const string Period6 = "LnrPrd6Pay";
+        public const string Period7 = "LnrPrd7Pay";
+        public const string Period8 = "LnrPrd8Pay";
+        public const string Period9 = "LnrPrd9Pay";
+        public const string Period10 = "LnrPrd10Pay";
+        public const string Period11 = "LnrPrd11Pay";
+        public const string Period12 = "LnrPrd12Pay";
+
+        // LearningDelivery
+        public const string AimSeqNumber = "AimSeqNumber";
+        public const string AimType = "AimType";
+        public const string AwardOrgCode = "AwardOrgCode";
+        public const string CompStatus = "CompStatus";
+        public const string EFACOFType = "EFACOFType";
+        public const string FundModel = "FundModel";
+        public const string LearnActEndDate = "LearnActEndDate";
+        public const string LearnAimRef = "LearnAimRef";
+        public const string LearnAimRefTitle = "LearnAimRefTitle";
+        public const string LearnAimRefType = "LearnAimRefType";
+        public const string LearnPlanEndDate = "LearnPlanEndDate";
+        public const string LearnStartDate = "LearnStartDate";
+        public const string LrnDelFAM_SOF = "LrnDelFAM_SOF";
+        public const string LearnDelFAM_LDM1 = "LearnDelFAM_LDM1";
+        public const string LearnDelFAM_LDM2 = "LearnDelFAM_LDM2";
+        public const string LearnDelFAM_LDM3 = "LearnDelFAM_LDM3";
+        public const string LearnDelFAM_LDM4 = "LearnDelFAM_LDM4";
+        public const string ProgType = "ProgType";
+        public const string SectorSubjectAreaTier2 = "SectorSubjectAreaTier2";
+        public const string WithdrawReason = "WithdrawReason";
+
+        // DP Outcome
+        public const string OutCode = "OutCode";
+        public const string OutType = "OutType";
+
+        // LARS Validity
+        public const string ValidityCategory = "ValidityCategory";
+        public const string ValidityLastNewStartDate = "ValidityLastNewStartDate";
+        public const string ValidityStartDate = "ValidityStartDate";
+
+        // FAM Types
+        public const string ECF = "ECF";
+        public const string EHC = "EHC";
+        public const string HNS = "HNS";
+        public const string MCF = "MCF";
+        public const string EDF = "EDF";
+        public const string LDM = "LDM";
+        public const string SOF = "SOF";
+    }
+}
--- src/FM25/ESFA.DC.ILR.FundingService.FM25.Service/Output/FundingOutputService.cs
+++ src/FM25/ESFA.DC.ILR.FundingService.FM25.Service/Output/FundingOutputService.cs
@@ -26,14 +26,14 @@ namespace ESFA.DC.ILR.FundingService.FM25.Service.Output
         {
             return new FM25Global()
             {
-                LARSVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.LARSVersion),
-                OrgVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.OrgVersion),
-                PostcodeDisadvantageVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.PostcodeDisadvantageVersion),
-                RulebaseVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.RulebaseVersion),
-                UKPRN = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.UKPRN),
+                LARSVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.LARSVersion),
+                OrgVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.OrgVersion),
+                PostcodeDisadvantageVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.PostcodeDisadvantageVersion),
+                RulebaseVersion = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.RulebaseVersion),
+                UKPRN = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, Attributes.UKPRN),
                 Learners = dataEntity
                     .Children?
-                    .Where(c => c.EntityName == OutputAttributeNames.Learner)
+                    .Where(c => c.EntityName == Attributes.EntityLearner)
                     .Select(MapLearner)
                     .ToList()
             };
@@ -43,33 +43,33 @@ namespace ESFA.DC.ILR.FundingService.FM25.Service.Output
         {
             return new FM25Learner()
             {
-                AcadMonthPayment = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.AcadMonthPayment),
-                AcadProg = _dataEntityAttributeService.GetBoolAttributeValue(dataEntity, OutputAttributeNames.AcadProg),
-                ActualDaysILCurrYear = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.ActualDaysILCurrYear),
-                AreaCostFact1618Hist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.AreaCostFact1618Hist),
-                Block1DisadvUpliftNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.Block1DisadvUpliftNew),
-                Block2DisadvElementsNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.Block2DisadvElementsNew),
-                ConditionOfFundingEnglish = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.ConditionOfFundingEnglish),
-                ConditionOfFundingMaths = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.ConditionOfFundingMaths),
-                CoreAimSeqNumber = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.CoreAimSeqNumber),
-                FullTimeEquiv = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.FullTimeEquiv),
-                FundLine = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.FundLine),
-                LearnerActEndDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, OutputAttributeNames.LearnerActEndDate),
-                LearnerPlanEndDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, OutputAttributeNames.LearnerPlanEndDate),
-                LearnerStartDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, OutputAttributeNames.LearnerStartDate),
-                LearnRefNumber = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.LearnRefNumber),
-                NatRate = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.NatRate),
-                OnProgPayment = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.OnProgPayment),
-                PlannedDaysILCurrYear = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.PlannedDaysILCurrYear),
-                ProgWeightHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.ProgWeightHist),
-                ProgWeightNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.ProgWeightNew),
-                PrvDisadvPropnHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.PrvDisadvPropnHist),
-                PrvHistLrgProgPropn = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.PrvHistLrgProgPropn),
-                PrvRetentFactHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.PrvRetentFactHist),
-                RateBand = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, OutputAttributeNames.RateBand),
-                RetentNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, OutputAttributeNames.RetentNew),
-                StartFund = _dataEntityAttributeService.GetBoolAttributeValue(dataEntity, OutputAttributeNames.StartFund),
-                ThresholdDays = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, OutputAttributeNames.ThresholdDays)
+                AcadMonthPayment = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, Attributes.AcadMonthPayment),
+                AcadProg = _dataEntityAttributeService.GetBoolAttributeValue(dataEntity, Attributes.AcadProg),
+                ActualDaysILCurrYear = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, Attributes.ActualDaysILCurrYear),
+                AreaCostFact1618Hist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.AreaCostFact1618Hist),
+                Block1DisadvUpliftNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.Block1DisadvUpliftNew),
+                Block2DisadvElementsNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.Block2DisadvElementsNew),
+                ConditionOfFundingEnglish = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.ConditionOfFundingEnglish),
+                ConditionOfFundingMaths = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.ConditionOfFundingMaths),
+                CoreAimSeqNumber = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, Attributes.CoreAimSeqNumber),
+                FullTimeEquiv = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.FullTimeEquiv),
+                FundLine = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.FundLine),
+                LearnerActEndDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, Attributes.LearnerActEndDate),
+                LearnerPlanEndDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, Attributes.LearnerPlanEndDate),
+                LearnerStartDate = _dataEntityAttributeService.GetDateTimeAttributeValue(dataEntity, Attributes.LearnerStartDate),
+                LearnRefNumber = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.LearnRefNumber),
+                NatRate = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.NatRate),
+                OnProgPayment = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.OnProgPayment),
+                PlannedDaysILCurrYear = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, Attributes.PlannedDaysILCurrYear),
+                ProgWeightHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.ProgWeightHist),
+                ProgWeightNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.ProgWeightNew),
+                PrvDisadvPropnHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.PrvDisadvPropnHist),
+                PrvHistLrgProgPropn = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.PrvHistLrgProgPropn),
+                PrvRetentFactHist = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.PrvRetentFactHist),
+                RateBand = _dataEntityAttributeService.GetStringAttributeValue(dataEntity, Attributes.RateBand),
+                RetentNew = _dataEntityAttributeService.GetDecimalAttributeValue(dataEntity, Attributes.RetentNew),
+                StartFund = _dataEntityAttributeService.GetBoolAttributeValue(dataEntity, Attributes.StartFund),
+                ThresholdDays = _dataEntityAttributeService.GetIntAttributeValue(dataEntity, Attributes.ThresholdDays)
             };
         }
     }
