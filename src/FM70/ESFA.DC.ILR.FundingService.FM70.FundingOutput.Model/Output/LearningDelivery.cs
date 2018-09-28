a0c215b32480fc44fcfda2db9658c7f15cdef607
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-28 10:49:34 +0100

    56334 - FM70 Output Models added.

---
+++ src/FM70/ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model/Output/LearningDelivery.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model.Output
+{
+    public class LearningDelivery
+    {
+        public int? AimSeqNumber { get; set; }
+
+        public LearningDeliveryValue LearningDeliveryValue { get; set; }
+
+        public List<LearningDeliveryDeliverableValues> LearningDeliveryDeliverableValues { get; set; }
+    }
+}
