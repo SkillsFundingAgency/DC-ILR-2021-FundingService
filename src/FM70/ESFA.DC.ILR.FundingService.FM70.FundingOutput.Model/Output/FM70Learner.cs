8304fc010c08c0814d47eaab834bdee9aac3496c
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-28 10:49:34 +0100

    56334 - FM70 Output Models added.

---
+++ src/FM70/ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model/Output/FM70Learner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model.Output
+{
+    public class FM70Learner
+    {
+        public string LearnRefNumber { get; set; }
+
+        public List<LearningDelivery> LearningDeliveries { get; set; }
+    }
+}
