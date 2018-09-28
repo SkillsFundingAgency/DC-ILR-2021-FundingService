6b9d1a6a4475e1d5d6cce50ed89029865b41b96a
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-28 10:49:34 +0100

    56334 - FM70 Output Models added.

---
+++ src/FM70/ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model/Output/LearningDeliveryValue.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model.Output
+{
+    public class LearningDeliveryValue
+    {
+        public DateTime? AchApplicDate { get; set; }
+
+        public bool? Achieved { get; set; }
+
+        public decimal? AchieveElement { get; set; }
+
+        public bool? AchievePayElig { get; set; }
+
+        public decimal? AchievePayPctPreTrans { get; set; }
+
+        public decimal? AchPayTransHeldBack { get; set; }
+
+        public int? ActualDaysIL { get; set; }
+
+        public int? ActualNumInstalm { get; set; }
+
+        public int? ActualNumInstalmPreTrans { get; set; }
+
+        public int? ActualNumInstalmTrans { get; set; }
+
+        public DateTime? AdjLearnStartDate { get; set; }
+
+        public bool? AdltLearnResp { get; set; }
+
+        public int? AgeAimStart { get; set; }
+
+        public decimal? AimValue { get; set; }
+
+        public DateTime? AppAdjLearnStartDate { get; set; }
+
+        public decimal? AppAgeFact { get; set; }
+
+        public bool? AppATAGTA { get; set; }
+
+        public bool? AppCompetency { get; set; }
+
+        public bool? AppFuncSkill { get; set; }
+
+        public decimal? AppFuncSkill1618AdjFact { get; set; }
+
+        public bool? AppKnowl { get; set; }
+
+        public DateTime? AppLearnStartDate { get; set; }
+
+        public DateTime? ApplicEmpFactDate { get; set; }
+
+        public DateTime? ApplicFactDate { get; set; }
+
+        public DateTime? ApplicFundRateDate { get; set; }
+
+        public string ApplicProgWeightFact { get; set; }
+
+        public decimal? ApplicUnweightFundRate { get; set; }
+
+        public decimal? ApplicWeightFundRate { get; set; }
+
+        public bool? AppNonFund { get; set; }
+
+        public decimal? AreaCostFactAdj { get; set; }
+
+        public int? BalInstalmPreTrans { get; set; }
+
+        public decimal? BaseValueUnweight { get; set; }
+
+        public decimal? CapFactor { get; set; }
+
+        public decimal? DisUpFactAdj { get; set; }
+
+        public bool? EmpOutcomePayElig { get; set; }
+
+        public decimal? EmpOutcomePctHeldBackTrans { get; set; }
+
+        public decimal? EmpOutcomePctPreTrans { get; set; }
+
+        public bool? EmpRespOth { get; set; }
+
+        public bool? ESOL { get; set; }
+
+        public bool? FullyFund { get; set; }
+
+        public string FundLine { get; set; }
+
+        public bool? FundStart { get; set; }
+
+        public int? LargeEmployerID { get; set; }
+
+        public decimal? LargeEmployerFM35Fctr { get; set; }
+
+        public DateTime? LargeEmployerStatusDate { get; set; }
+
+        public decimal? LTRCUpliftFctr { get; set; }
+
+        public decimal? NonGovCont { get; set; }
+
+        public bool? OLASSCustody { get; set; }
+
+        public decimal? OnProgPayPctPreTrans { get; set; }
+
+        public int? OutstndNumOnProgInstalm { get; set; }
+
+        public int? OutstndNumOnProgInstalmTrans { get; set; }
+
+        public int? PlannedNumOnProgInstalm { get; set; }
+
+        public int? PlannedNumOnProgInstalmTrans { get; set; }
+
+        public int? PlannedTotalDaysIL { get; set; }
+
+        public int? PlannedTotalDaysILPreTrans { get; set; }
+
+        public decimal? PropFundRemain { get; set; }
+
+        public decimal? PropFundRemainAch { get; set; }
+
+        public bool? PrscHEAim { get; set; }
+
+        public bool? Residential { get; set; }
+
+        public bool? Restart { get; set; }
+
+        public decimal? SpecResUplift { get; set; }
+
+        public decimal? StartPropTrans { get; set; }
+
+        public int? ThresholdDays { get; set; }
+
+        public bool? Traineeship { get; set; }
+
+        public bool? Trans { get; set; }
+
+        public DateTime? TrnAdjLearnStartDate { get; set; }
+
+        public bool? TrnWorkPlaceAim { get; set; }
+
+        public bool? TrnWorkPrepAim { get; set; }
+
+        public decimal? UnWeightedRateFromESOL { get; set; }
+
+        public decimal? UnweightedRateFromLARS { get; set; }
+
+        public decimal? WeightedRateFromESOL { get; set; }
+
+        public decimal? WeightedRateFromLARS { get; set; }
+    }
+}
