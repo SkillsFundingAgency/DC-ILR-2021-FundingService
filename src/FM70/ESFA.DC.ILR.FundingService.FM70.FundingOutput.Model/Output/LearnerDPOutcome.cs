0b66d29206b8331579ef14b52b0783cfa1d59864
Ashley Beecham <ash@momentoussolutions.co.uk>
2018-09-28 10:56:44 +0100

    56334 - Adding DP Outcome Output Model

---
+++ src/FM70/ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model/Output/LearnerDPOutcome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ESFA.DC.ILR.FundingService.FM70.FundingOutput.Model.Output
+{
+    public class LearnerDPOutcome
+    {
+        public int OutCode { get; set; }
+
+        public string OutType { get; set; }
+
+        public DateTime OutStartDate { get; set; }
+
+        public DateTime? OutcomeDateForProgression { get; set; }
+
+        public bool? PotentialESFProgressionType { get; set; }
+
+        public string ProgressionType { get; set; }
+
+        public bool? ReachedSixMonthPoint { get; set; }
+
+        public bool? ReachedThreeMonthPoint { get; set; }
+
+        public bool? ReachedTwelveMonthPoint { get; set; }
+    }
+}
