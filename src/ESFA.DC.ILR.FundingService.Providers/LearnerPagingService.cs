57a1b4e6bc591c8f9318bd2069bddf2bc9c1e012
Hal Angseesing <tomlingames@gmail.com>
2018-10-11 13:08:36 +0100

    Perf Test: Changed the paging size (to 15000) to minimise actor creation in funding service

--- src/ESFA.DC.ILR.FundingService.Providers/LearnerPagingService.cs
+++ src/ESFA.DC.ILR.FundingService.Providers/LearnerPagingService.cs
@@ -8,7 +8,7 @@ namespace ESFA.DC.ILR.FundingService.Stubs
     public class LearnerPagingService<T> : IPagingService<T>
         where T : class
     {
-        private const int PageSize = 1500;
+        private const int PageSize = 15000;
 
         private readonly IFundingContext _fundingContext;
 
